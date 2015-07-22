634093754616b26ada71fce6c0d9a227164e25e8
Una Gauper <umgauper@gmail.com>
2015-07-22 12:20:53 -0700

    added functions getSymbols, addSymbol, deleteSymbol, constructQuery, and addStock

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,22 +1,91 @@
 'use strict';
 
 angular.module('stockjumpApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', function ($scope, $http, chartService) {
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
+
+    var seriesOptions = [];
+
+    $scope.stockSymbols = [];
+
+    $scope.getSymbols = function() { //rename? getSymbols and update chart?
+      $http.get('/api/symbols').success(function (data) {
+        console.log(data);
+        $scope.allSymbols = data;
+
+        //update stockSymbols
+        $scope.stockSymbols = [];
+
+        $scope.allSymbols.forEach(function(el) {
+          $scope.stockSymbols.push(el.symbol);
+        });
+
+        //$scope.constructQuery($scope.stockSymbols);
+        $scope.addStock($scope.constructQuery($scope.stockSymbols));
+
+      });
+    };
+
+
+
+    $scope.addSymbol = function(symbol) {
+        $http.post('/api/symbols', {symbol: symbol}).success(function() {
+          $scope.getSymbols(); //udpate list of symbols
+        });
+    };
+
+
+    $scope.deleteSymbol = function(_id) {
+      $http.delete('/api/symbols/' + _id).success(function() {
+        $('#' +_id).remove();
+      });
+    };
+
+    $scope.constructQuery = function(stockSymbols) {
+      var query = '';
+      var queryBeginning = 'https://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol in';
+      var queryEnding = 'and startDate = "2015-01-01" and endDate = "2015-07-20"&format=json&diagnostics=true&env=http://datatables.org/alltables.env&format=json';
+      //TODO: change endDate in query to reflect todays date..using Date method...
+      var queryMiddle = '(';
+
+      for (var i = 0; i < stockSymbols.length - 1; i++) {
+        queryMiddle = queryMiddle + '"' + stockSymbols[i] + '"' + ", ";
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+
+      queryMiddle = queryMiddle + '"' + stockSymbols[stockSymbols.length-1] + '")';
+
+      query = queryBeginning + queryMiddle + queryEnding;
+      return query;
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+
+    $scope.addStock = function(query) {
+
+      $http.get(query).success(function (data) {
+        seriesOptions.push({name: data.query.results.quote[0].Symbol, data: []});
+
+        //seriesOptions[0].name = data.query.results.quote[0].Symbol;
+        var j = 0;
+
+        for(var i = 0; i < data.query.results.quote.length; i++) {
+          if (seriesOptions[j].name === data.query.results.quote[i].Symbol) {
+            var arr = [];
+            arr.push(Date.parse(data.query.results.quote[i].Date));
+            arr.push(Number(data.query.results.quote[i].Open));
+
+            seriesOptions[j].data.push(arr);
+          } else {
+            j++;
+            seriesOptions.push({name: data.query.results.quote[i].Symbol,
+                                data: [[Date.parse(data.query.results.quote[i].Date), Number(data.query.results.quote[i].Open)]]
+                              }
+            );
+          }
+        }
+        chartService.makeChart(seriesOptions);
+      });
     };
+
+
   });
